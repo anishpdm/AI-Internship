52e3e208e73d45d8686bc4d5c2a5f5e7c4dd6651
anishpdm <anish.vilayil.s@gmail.com>
2025-06-19 20:44:34 +0530

    Evaluation

--- ml_predict.py
+++ ml_predict.py
@@ -1,6 +1,19 @@
 import joblib
-import numpy as np
-pred_value = np.array( [[160]] ) 
-result_model = joblib.load("my_model.pkl")
-result= result_model.predict(pred_value)
+import pandas as pd
+
+
+df = pd.read_csv("new_data.csv")
+
+X = df[ ["height"] ]
+
+pred_value = pd.DataFrame( [[160]] , columns=X.columns ) 
+result_model_lr = joblib.load("lr_model.pkl")
+result_model_knn = joblib.load("knn_model.pkl")
+
+
+result= result_model_lr.predict(pred_value)
 print(result)
+
+
+result_knn= result_model_knn.predict(pred_value)
+print(result_knn)
